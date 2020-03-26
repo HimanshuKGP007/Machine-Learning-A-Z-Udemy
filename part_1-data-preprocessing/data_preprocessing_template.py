3772a839d89217c503247608f7e138f07e7ca830
Himanshu <49796034+HimanshuKGP007@users.noreply.github.com>
2020-03-26 14:13:25 +0530

    Update data_preprocessing_template.py

--- part_1-data-preprocessing/data_preprocessing_template.py
+++ part_1-data-preprocessing/data_preprocessing_template.py
@@ -7,14 +7,14 @@ import pandas as pd
 # Importing the dataset
 # ---------------------
 dataset = pd.read_csv('../data_files/Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+X = dataset.iloc[:, :-1] # df
+y = dataset.iloc[:, 3] #pandas series
 
 # Taking care of missing data
 # ---------------------------
-from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
-imputer = imputer.fit(X[:, 1:3])
+from sklearn.preprocessing import SimpleImputer
+imputer = SimpleImputer(missing_values='NaN', strategy='mean', verbose =0)
+imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 # Encoding categorical data
