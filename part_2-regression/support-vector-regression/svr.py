96f476ed2cac5084c76b9b65d43f50ea7993290d
Himanshu <49796034+HimanshuKGP007@users.noreply.github.com>
2020-04-11 19:20:16 +0530

    Update svr.py

--- part_2-regression/support-vector-regression/svr.py
+++ part_2-regression/support-vector-regression/svr.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 # Importing the dataset
 # ---------------------
-dataset = pd.read_csv('../../data_files/Position_Salaries.csv')
+dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
@@ -15,29 +15,23 @@ from sklearn.preprocessing import StandardScaler
 
 sc_X = StandardScaler()
 sc_y = StandardScaler()
+temp = np.array(y).reshape((-1, 1))#This was done for converting the vector Y into a matrix for feature scaling
+X = sc_X.fit_transform(X)     
+y = sc_y.fit_transform(temp)
 
-X = sc_X.fit_transform(X)
-y = sc_y.fit_transform(y)
 
-# Fitting SVR to the dataset
+# Fitting Regression to the dataset - SVR
 from sklearn.svm import SVR
 regressor = SVR(kernel='rbf')
 regressor.fit(X, y)
-
+# create your regressor here
 # Predicting a new result with SVR
-y_pred = sc_y.inverse_transform(
-	regressor.predict(
-		sc_X.transform(
-			np.array([[6.5]])
-		)
-	)
-)
-
-# Visualising the SVR results
-X_grid = np.arange(min(X), max(X), 0.1)
-X_grid = X_grid.reshape((len(X_grid), 1))
+y_pred = sc_y.inverse_transform(regressor.predict(sc_X.transform(np.array([[6.5]]))))
+       
+# Visualising the Polynomial Regression results (for higher resolution and smoother curve)
+# Visualising the Polynomial Regression results
 plt.scatter(X, y, color='red')
-plt.plot(X_grid, regressor.predict(X_grid), color='blue')
+plt.plot(X, regressor.predict(X), color='blue')
 plt.title('Truth or Bluff (SVR)')
 plt.xlabel('Position')
 plt.ylabel('Salary')
