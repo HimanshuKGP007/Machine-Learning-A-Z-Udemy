3f0851b5c0aa170f7164a34cad60e00f5af7219a
Himanshu <49796034+HimanshuKGP007@users.noreply.github.com>
2020-04-16 11:20:42 +0530

    Update random_forest_regression.py

--- part_2-regression/random-forest-regression/random_forest_regression.py
+++ part_2-regression/random-forest-regression/random_forest_regression.py
@@ -3,27 +3,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 
 # Importing the dataset
 # ---------------------
-dataset = pd.read_csv('../../data_files/Position_Salaries.csv')
+dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
 # Fitting Regression to the dataset
 from sklearn.ensemble import RandomForestRegressor
 regressor = RandomForestRegressor(n_estimators=300, random_state=0)
-regressor.fit(X, y)
+rnd_clf = RandomForestClassifier(n_estimators=500, n_jobs=-1, random_state=0)
+model = regressor.fit(X, y)
 
 # Predicting a new result with Polynomial Regression
-y_pred = regressor.predict(6.5)
+y_pred = regressor.predict(np.array([[6.5]]))
 
 # Visualising the Polynomial Regression results (for higher resolution and smoother curve)
-X_grid = np.arange(min(X), max(X), 0.01)
-X_grid = X_grid.reshape(len(X_grid), 1)
 plt.scatter(X, y, color='red')
-plt.plot(X_grid, regressor.predict(X_grid), color='blue')
+plt.plot(X, regressor.predict(X), color='blue')
 plt.title('Truth or Bluff (Random Forest Regression)')
 plt.xlabel('Position')
 plt.ylabel('Salary')
 plt.show()
+
+#Feature Importance
+importances = model.feature_importances_
+
+#Look for Importances
+
+
+
